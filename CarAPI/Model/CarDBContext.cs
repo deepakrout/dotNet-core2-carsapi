b5959fa99526bdebf5160ee045578272a994c59e
deepakrout <deepakrout@hotmail.com>
2018-09-19 23:13:46 -0400

    Initial and final commit

---
+++ CarAPI/Model/CarDBContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace CarAPI.Model
+{
+    public class CarDBContext : DbContext
+    {
+        public CarDBContext(DbContextOptions<CarDBContext> options) : base (options)
+        {
+
+        }
+        public DbSet<Car> Cars { get; set; }
+    }
+}
